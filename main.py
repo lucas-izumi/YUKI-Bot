fdca18bf910908026f45d79581185376a69ebf85
Lucas Izumi <lucasizumi@gmail.com>
2021-01-14 04:13:17 +0000

    Formatando a mensagem do bot

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ async def on_message(message):
   if message.content.startswith('$combineroles'):
     guild_id_ = message.guild.id
     # necessario dar suporte a cargos com espaco no nome
+    # criar persistencia
     rolelistnames = message.content.replace("\"", "").split()[1:]
     if len(rolelistnames) != 3:
       await message.channel.send('Sintaxe: $combineroles "cargo1" "cargo2" "cargocombinado"')
@@ -35,7 +36,7 @@ async def on_message(message):
           rolelist_[1] = role.id
         if role.name == rolelistnames[2]:
           rolelist_[2] = role.id
-      await message.channel.send(rolelistnames[2] + " agora é uma combinação dos cargos " + rolelistnames[0] + " e " + rolelistnames[1])
+      await message.channel.send("**" + rolelistnames[2] + "**" + " agora é uma combinação dos cargos **" + rolelistnames[0] + "** e **" + rolelistnames[1] + "**")
 
 @client.event
 async def on_member_update(before, after):
